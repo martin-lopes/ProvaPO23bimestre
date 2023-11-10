2212c83d9acfd87de8dbb8c99b80828c2c3e2573
Bruna Rodrigues Lopes <2022102020001@aluno.ifro.edu.br>
2023-11-10 14:31:44 -0400

    Terceira versão (rascunho da lógica)

--- Prova/FolhaDePagamento.cs
+++ Prova/FolhaDePagamento.cs
@@ -1,20 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class FolhaDePagamento
 {
+    public List<FolhaDePagamento> FolhaDePagamentoList; 
     private string mes;
     public Funcionario f2;
     private double valoriprf;
     private double horaextrastrab;
-    private double pagamento;
+    private static double pagamento;
+    private double beneficios;
 
     public string Mes { get { return mes; } set { mes = value; } }
     public double Valoriprf { get {  return valoriprf; } set {  valoriprf = value; } }
     public double Horaextrastrab { get {  return horaextrastrab; } set {  horaextrastrab = value; } }
-    public double Pagamento { get { return pagamento; } set { pagamento = value; } }
+    public static double Pagamento { get { return pagamento; } set { pagamento = value; } }
+    public double Beneficios { get { return beneficios; } set { beneficios = value; } }
 
-    /*public double CalcPagamento(double pagamento, double salario)
+    public double CalcPagamento(double pagamento, double salario)
     {
-        
-    }*/
+        salario = salario + beneficios;//teoria apenas
+        return salario;
+    }
+
+    public double iprf(double salario, double valoriprf)
+    {
+        if (salario > 5000) { valoriprf = salario - (salario * 0.17); }  else { } return valoriprf;
+    }
+
+    public static double ValorGastoPag(string mes, List<FolhaDePagamento> FolhaDePagamentoList)
+    {
+        double valorgastopag = 0.0;
+        return valorgastopag;
+    }
 }
